fc8437c69000272ec6b5dd707be9b534be9a21a0
Vladi <VladPetrovArtem@mail.ru>
2023-10-13 01:17:42 +0300

    Мой проект

---
+++ main.py
@@ -0,0 +1,15 @@
+import math
+import pprint
+from typing import Tuple
+
+print("Wussap")
+
+def foo(x: int) -> Tuple[int, float]:
+    return x, math.pi
+
+
+pprint.pprint('Wussssap')
+
+
+
+print(foo(4))
